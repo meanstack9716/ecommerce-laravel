11b236305c306a75baca273a384e5d34546b5cc1
meanstack9716 <meanstack9@gmail.com>
2025-05-16 17:42:15 +0530

    changes in enum for constants

--- app/Constants/constants.php
+++ app/Constants/constants.php
@@ -11,7 +11,13 @@ class Constants
     public const SELLER_ROLE = "Seller";
     public const STATUS_APPROVED = "Approved";
     public const STATUS_PENDING = "Pending";
+    public const STATUS_CONFIRMED = "Confirmed";
+    public const STATUS_PROCESSING = "Processing";
+    public const STATUS_SHIPPED = "Shipped";
+    public const STATUS_DELIVERED = "Delivered";
     public const STATUS_CANCELLED = "Cancelled";
+    public const STATUS_RETURNED = "Returned";
+    public const STATUS_REFUNDED = "Refunded";
     public const STATUS_RECEIVED = "Received";
     public const STATUS_REJECTED = "Rejected";
     public const STATUS_VERIFIED = "Verified";
